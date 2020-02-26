85997a62fea8ffd1a4c60a56e7d5ee0bc7489f74
Peter Bedrosian <peter_bedrosian@yahoo.com>
2020-02-26 18:22:46 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,29 @@
+#class Dog
+#  attr_accessor :breed, :name
+#  def initialize(breed = "Mutt")
+#    @breed = breed
+#  end
+#end
+
 class Dog
-  attr_accessor :breed, :name
   def initialize(breed = "Mutt")
     @breed = breed
   end
+
+  def breed= (breed)
+    @breed
+  end
+
+  def breed
+    @breed
+  end
+
+  def name= (name)
+    @name
+  end
+
+    def name
+      @name
+    end
+
 end
